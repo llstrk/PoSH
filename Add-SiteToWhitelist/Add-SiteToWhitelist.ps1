26ba2d748998fc505d3fc13e6767780b51965161
llstrk <dryerr@gmail.com>
2016-07-13 10:47:07 +0200

    First attempt at splitting script into functions

--- Add-SiteToWhitelist/Add-SiteToWhitelist.ps1
+++ Add-SiteToWhitelist/Add-SiteToWhitelist.ps1
@@ -1,79 +1,135 @@
-$GPOName = 'TestMKS-Override'
-$WhitelistPath = 'C:\GPO\Whitelist.csv'
-
-
-# Remove previous values from ProxyOverride
-$params = @{
-    'Name'="$GPOName";
-    'Context'='User';
-    'Key'='HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
-    'ValueName'='ProxyOverride';
-}
-Remove-GPPrefRegistryValue @params
-
-# Add new values to ProxyOverride
-$params = @{
-    'Name'="$GPOName";
-    'Action'='Replace';
-    'Context'='User';
-    'Key'='HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings';
-    'Type'='String';
-    'ValueName'='ProxyOverride';
-    'Value'=$($Websites.Url) -join ';'
-}
-Set-GPPrefRegistryValue @params
-
-# Add links to bookmarks bar
-$GPOItem = Get-GPO -Name $GPOName
-
-# Get contents from Shortcuts.xml in the GPO specified in variable $GPOName
-[xml]$GPO = Get-Content "\\$Env:USERDOMAIN\SYSVOL\$Env:USERDNSDOMAIN\Policies\{$($GPOItem.Id.Guid)}\User\Preferences\Shortcuts\Shortcuts.xml"
-
-# First, remove any old values from the XML in Shortcuts element 
-$ChildNodes = $GPO.SelectNodes('//Shortcuts/Shortcut')
-foreach ($Child in $ChildNodes) {
-    [void]$Child.ParentNode.RemoveChild($Child)
+function RemoveProxyOverride {
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$GPOName
+    )
+    # Remove previous values from ProxyOverride
+    $params = @{
+        'Name'="$GPOName";
+        'Context'='User';
+        'Key'='HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+        'ValueName'='ProxyOverride';
+    }
+    Remove-GPPrefRegistryValue @params
 }
 
-# Add each bookmark to XML in Shortcuts element
-$Websites = Import-Csv -Path $WhitelistPath -Delimiter ';'
-foreach ($Website in $Websites) {
-    $Timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
-    $NodeId = ([guid]::NewGuid()).Guid
+function Set-GpoPreferenceProxyOverrideUrl {
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$GPOName,
+
+        [Parameter(Mandatory)]
+        [string[]]$Url
+    )
+    # Add new values to ProxyOverride
+    $params = @{
+        'Name'="$GPOName";
+        'Action'='Replace';
+        'Context'='User';
+        'Key'='HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings';
+        'Type'='String';
+        'ValueName'='ProxyOverride';
+        'Value'=$Url -join ';'
+    }
     
-    # Create new XML element 'Shortcut'
-    $NewShortcut = $GPO.CreateElement("Shortcut")
-    $NewShortcut.SetAttribute('clsid',"{4F2F7C55-2790-433e-8127-0739D1CFA327}")
-    $NewShortcut.SetAttribute('name',"$($Website.Name)")
-    $NewShortcut.SetAttribute('status',"$($Website.Name)")
-    $NewShortcut.SetAttribute('image','1')
-    $NewShortcut.SetAttribute('changed',"$Timestamp")
-    $NewShortcut.SetAttribute('uid',"{$NodeId}")
-    $NewShortcut.SetAttribute('removePolicy','1')
-    $NewShortcut.SetAttribute('userContext','1')
-    $NewShortcut.SetAttribute('bypassErrors','1')
-    
-    # Create new XML element 'Properties'
-    $NewProperty = $GPO.CreateElement("Properties")
-    $NewProperty.SetAttribute('pidl','')
-    $NewProperty.SetAttribute('targetType','URL')
-    $NewProperty.SetAttribute('action','R')
-    $NewProperty.SetAttribute('comment','')
-    $NewProperty.SetAttribute('shortcutKey','0')
-    $NewProperty.SetAttribute('startIn','')
-    $NewProperty.SetAttribute('arguments','')
-    $NewProperty.SetAttribute('iconIndex','0')
-    $NewProperty.SetAttribute('targetPath',$($Website.Url))
-    $NewProperty.SetAttribute('iconPath','')
-    $NewProperty.SetAttribute('window','')
-    $NewProperty.SetAttribute('shortcutPath',"%FavoritesDir%\Links\$($Website.Name)")
-    
-    # Append new XML child element 'Shortcut' to element 'Shortcuts'
-    $GPO.Shortcuts.AppendChild($NewShortcut)
+    RemoveProxyOverride -GPOName $GPOName
 
-    # Append new XML child element 'Properties' to element where 'Shortcut' uid matches $NodeID
-    @($GPO.Shortcuts.Shortcut).Where({$_.uid -like "*$NodeID*"}).AppendChild($NewProperty)
+    Set-GPPrefRegistryValue @params
 }
 
-# Save changes to Shortcuts.xml in the GPO specified in variable $GPOName
-$GPO.Save("\\$Env:USERDOMAIN\SYSVOL\$Env:USERDNSDOMAIN\Policies\{$($GPOItem.Id.Guid)}\User\Preferences\Shortcuts\Shortcuts.xml")
+function GetGpoPreferenceXmlFile {
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$GPOName,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('Shortcut')]
+        [string]$PreferenceType,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('User','Computer')]
+        [string]$Context
+    )
+
+    $GPOItem = Get-GPO -Name $GPOName
+
+    $prefRoot = "\\$Env:USERDOMAIN\SYSVOL\$Env:USERDNSDOMAIN\Policies\{$($GPOItem.Id.Guid)}\User\Preferences"
+    
+    switch ($PreferenceType) {
+        'Shortcut' {
+            $xmlFilePath = Join-Path $prefRoot 'Shortcuts\Shortcut.xml'
+        }
+    }
+
+    [xml](Get-Content -Path $xmlFilePath)
+}
+
+
+function RemoveGpoPreferenceChildNodesFromXml {
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory)]
+        [xml]$Xml
+    )
+    # First, remove the old values
+    $ChildNodes = $Xml.SelectNodes('//Shortcuts/Shortcut')
+    foreach ($Child in $ChildNodes) {
+        [void]$Child.ParentNode.RemoveChild($Child)
+    }
+}
+
+function Set-GpoPreferenceShortcut {
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$GPOName,
+
+        [Parameter(Mandatory)]
+        [string[]]$Url
+    )
+    # Add each link to bookmarks bar
+
+    $gpo = GetGpoPreferenceXmlFile -GPOName $GPOName -PreferenceType Shortcut -Context User
+
+    RemoveGpoPreferenceChildNodesFromXml -Xml $gpo
+
+    foreach ($Website in $Url) {
+        $Timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+        $NodeId = ([guid]::NewGuid()).Guid
+        
+        # Create XML element Shortcut
+        $NewShortcut = $gpo.CreateElement("Shortcut")
+        $NewShortcut.SetAttribute('clsid',"{4F2F7C55-2790-433e-8127-0739D1CFA327}")
+        $NewShortcut.SetAttribute('name',"$($Website.Name)")
+        $NewShortcut.SetAttribute('status',"$($Website.Name)")
+        $NewShortcut.SetAttribute('image','1')
+        $NewShortcut.SetAttribute('changed',"$Timestamp")
+        $NewShortcut.SetAttribute('uid',"{$NodeId}")
+        $NewShortcut.SetAttribute('removePolicy','1')
+        $NewShortcut.SetAttribute('userContext','1')
+        $NewShortcut.SetAttribute('bypassErrors','1')
+        
+        # Create XML element Properties
+        $NewProperty = $gpo.CreateElement("Properties")
+        $NewProperty.SetAttribute('pidl','')
+        $NewProperty.SetAttribute('targetType','URL')
+        $NewProperty.SetAttribute('action','R')
+        $NewProperty.SetAttribute('comment','')
+        $NewProperty.SetAttribute('shortcutKey','0')
+        $NewProperty.SetAttribute('startIn','')
+        $NewProperty.SetAttribute('arguments','')
+        $NewProperty.SetAttribute('iconIndex','0')
+        $NewProperty.SetAttribute('targetPath',$($Website.Url))
+        $NewProperty.SetAttribute('iconPath','')
+        $NewProperty.SetAttribute('window','')
+        $NewProperty.SetAttribute('shortcutPath',"%FavoritesDir%\Links\$($Website.Name)")
+        
+        $gpo.Shortcuts.AppendChild($NewShortcut)
+        @($gpo.Shortcuts.Shortcut).Where({$_.uid -like "*$NodeID*"}).AppendChild($NewProperty)
+    }
+
+    $gpo.Save("\\$Env:USERDOMAIN\SYSVOL\$Env:USERDNSDOMAIN\Policies\{$($GPOItem.Id.Guid)}\User\Preferences\Shortcuts\Shortcuts.xml")
+}
